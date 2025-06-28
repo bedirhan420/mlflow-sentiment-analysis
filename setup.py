f1c59a6fc644203a687edeb43e25e93392e2135c
bedirhan420 <bedirhancelik4343@gmail.com>
2025-06-29 00:32:24 +0300

    data ingestion and preprocessing with dvc is added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "youtube",
+    version = "0.0.0",
+    author = "bedirhan",
+    author_email="bedirhancelik4343@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
